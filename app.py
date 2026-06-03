c340ff277a4dcf8d324969968eb8c346ce5f7f69
Kirti Shinde <kirtishinde3520@gmail.com>
2026-06-03 18:56:57 +0530

    first flask server

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+# --------------
+# serevr 
+# --------------
+app = Flask(__name__)
+
+
+# --------------
+# Routes 🌍
+# --------------
+
+@app.route("/", methods=["GET"])
+def home():
+    return {"message" : "Welcome to flask 🌸"}
+
+# CREATE 
+@app.route("/todo/create", methods=["POST"])
+def createTodo():
+    return {"message" : "Todo create success ✚"}
+
+# READ 
+@app.route("/todos", methods=["GET"])
+def readTodo():
+    return {"message" : "Todo readTodo success 👀"}
+
+
+# UPDATE 
+@app.route("/todo/update", methods=["PUT"])
+def updateTodo():
+    return {"message" : "Todo update success ✏️"}
+
+
+# DELETE 
+@app.route("/todo/remove", methods=["DELETE"])
+def deleteTodo():
+    return {"message" : "Todo delete success ❌"}
+
+
+# -----------------
+# serevr start 🌍
+# -----------------
+app.run(debug=True)
